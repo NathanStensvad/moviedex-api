e42c3bfdac2d761784c7a8e679c43c8740ca0211
Nathan Stensvad <nstensvad@hotmail.com>
2020-10-02 01:08:49 -0700

    changes for heroku server

--- server.js
+++ server.js
@@ -7,19 +7,30 @@ const MOVIEDEX = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-  
+
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request' })
+        return res.status(401).json({ error: 'Unauthorized request' })
     }
     next()
-  })
+})
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIEDEX;
@@ -36,7 +47,7 @@ app.get('/movie', function handleGetMovie(req, res) {
         )
     }
 
-    if(req.query.avg_vote) {
+    if (req.query.avg_vote) {
         response = response.filter(movie =>
             movie.avg_vote >= req.query.avg_vote
         )
@@ -45,8 +56,8 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+
 })
